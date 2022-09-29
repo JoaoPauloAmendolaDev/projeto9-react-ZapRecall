3d070e6bf1b231d3af6e173017b413d5cd2bc0e1
João Paulo Silva Amendola <joaoa@id.uff.br>
2022-09-29 16:02:56 -0300

    versao semi-funcional

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,30 @@
 import styled from "styled-components";
 import { useState } from "react";
 import logo from "../assets/img/logo.png";
-import Deck from "./Deck"
+import Deck from "./Deck";
 
 export default function App() {
   return (
     <>
       <ScreenContainer>
         <LogoConteiner>
-          <img src={logo} />
+          <img src={logo} alt="logo ZapRecall" />
           <h1>ZapRecall</h1>
         </LogoConteiner>
-          <Deck />
+
+        <Deck />
       </ScreenContainer>
+      <Footer>
+        <ContainerBotoes>
+          <button>Não Lembro!</button>
+          <button>Quase não lembrei</button>
+          <button>Zap!</button>
+        </ContainerBotoes>
+        <p>0/4 concluídos</p>
+      </Footer>
     </>
   );
 }
-
 const ScreenContainer = styled.div`
    {
     background-color: #fb6b6b;
@@ -39,6 +47,7 @@ const LogoConteiner = styled.div`
     img {
       width: 52px;
     }
+
     h1 {
       font-family: "Righteous";
       font-style: normal;
@@ -50,4 +59,50 @@ const LogoConteiner = styled.div`
     }
   }
 `;
+const Footer = styled.div`
+  width: 100%;
+  min-height: 50px;
+  background-color: #ffffff;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: "Recursive";
+  font-weight: 400;
+  font-size: 18px;
+  color: #333333;
+  padding: 10px;
 
+  ContainerBotoes {
+    display: flex;
+    width: 80%;
+    justify-content: space-between;
+    margin: 20px;
+  }
+`;
+const ContainerBotoes = styled.div`
+  display: flex;
+  width: 80%;
+  justify-content: space-between;
+  margin: 20px;
+
+  button {
+    width: 90px;
+    font-family: "Recursive";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: #ffffff;
+    background: blue;
+    border-radius: 5px;
+    border: 1px solid blue;
+    padding: 5px;
+  }
+`;
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import styled from "styled-components";
+import setaPlay from "../assets/img/seta_play.png";
+import setaChange from "../assets/img/seta_virar.png";
 
 const PerguntaFechada = styled.div`
   width: 300px;
@@ -13,20 +16,87 @@ const PerguntaFechada = styled.div`
   justify-content: space-between;
 `;
 
-let questions = [
-  "O que é JSX?",
-  "Para que serve condicionais em JS?",
-  "Qual é a biblioteca de JS mais baixada no mundo?",
-  "Quantos tipos de funções existem em JS?",
+const OpenQuestion = styled.div`
+  width: 300px;
+  margin: 12px;
+  padding: 15px;
+  min-height: 100px;
+  background: #ffffd5;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  font-family: "Recursive";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 22px;
+  color: #333333;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  img {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+  }
+`;
+
+const questions = [
+  {
+    question: "O que é JSX?",
+    answer:
+      "JSX é uma mistura entre HTML e javascript, utilizado no React para facilitar a programação de páginas single-page",
+  },
+  { question: "Para que serve condicionais em JS?", answer: "Serve para tornar interativa uma página, dependendo de como o usuário interage" },
+  {
+    question: "Qual é a biblioteca de JS mais baixada no mundo?",
+    answer: "React.JS",
+  },
+  { question: "Quantos tipos de funções existem em JS?", answer: "4" },
 ];
 
 export default function Deck() {
+  let [listaRespostasAbertas, setListaRespostasAbertas] = useState([]);
+  let [listaPerguntasAbertas, setListaPerguntasAbertar] = useState([]);
+
+  function clicked(index) {
+    let newIndex = index;
+    console.log(index);
+    setListaPerguntasAbertar([...listaPerguntasAbertas, newIndex]);
+  }
+
+  function answerClicked(index) {
+    let newIndex = index;
+    setListaRespostasAbertas([...listaRespostasAbertas, newIndex]);
+  }
+
   return (
-    <>
-      {questions.map((question, index) => (
-        <PerguntaFechada>{question}</PerguntaFechada>
-      ))}
-    </>
+    <div>
+      {questions.map(({ question, answer }, index) =>
+        listaPerguntasAbertas.includes(index) ? (
+          <>
+            {listaRespostasAbertas.includes(index) ? (
+              <OpenQuestion>{answer}</OpenQuestion>
+            ) : (
+              <OpenQuestion key={index}>
+                {question}
+                <img
+                  src={setaChange}
+                  alt="Seta para virar"
+                  onClick={() => answerClicked(index)}
+                />
+              </OpenQuestion>
+            )}
+            {console.log(listaPerguntasAbertas)}
+          </>
+        ) : (
+          <PerguntaFechada key={index}>
+            <p>Pergunta {index}</p>
+            <img src={setaPlay} alt="Seta" onClick={() => clicked(index)} />
+          </PerguntaFechada>
+        )
+      )}
+    </div>
   );
 }
-
